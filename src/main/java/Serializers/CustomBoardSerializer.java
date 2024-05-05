8a3058b76360adb8d80e2bd46f1a43c7144eabcd
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-05-06 00:37:16 +0300

    added option to get accessed boards

---
+++ src/main/java/Serializers/CustomBoardSerializer.java
@@ -0,0 +1,28 @@
+package Serializers;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import DataModels.Board;
+
+public class CustomBoardSerializer extends StdSerializer<Board> {
+
+	public CustomBoardSerializer() {
+		this(null);
+	}
+
+	public CustomBoardSerializer(Class<Board> t) {
+		super(t);
+	}
+
+	@Override
+	public void serialize(Board board, JsonGenerator jgen, SerializerProvider provider) throws java.io.IOException {
+
+		jgen.writeStartObject();
+		jgen.writeNumberField("id", board.getBoardId());
+		jgen.writeStringField("name", board.getName());
+		jgen.writeEndObject();
+	}
+
+}
